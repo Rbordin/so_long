7ed2e38c0610676618fdc9a5f3e74a6c435e5799
Rbordin <123752418+Rbordin@users.noreply.github.com>
2023-03-13 10:19:28 +0100

    Add files via upload

--- so_long.c
+++ so_long.c
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rbordin <rbordin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: riccardobordin <riccardobordin@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 16:06:33 by rbordin           #+#    #+#             */
-/*   Updated: 2023/03/07 11:06:45 by rbordin          ###   ########.fr       */
+/*   Updated: 2023/03/11 16:09:16 by riccardobor      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "so_long.h"
 
-// 5 FUNZIONI: main, controllo del formato e hook
+// 5 FUNZIONI: main, controllo del formatoo e hook
 int	xclose()
 {
 	exit(0);
@@ -29,6 +29,57 @@ int	key_close(int keycode, t_vars *vars)
 	}
 	else
 		move(keycode, vars);
+			
+	return (0);
+}
+
+int	horizontal_control( int keycode, t_vars *vars)
+{
+	int x;
+	int y;
+	int	step;
+
+	step = 0;
+	x = vars->player_x;
+	while (vars->map[x])
+	{
+		y = vars->player_y;
+		while (vars->map[x][y] != 1)
+		{
+			if (vars->map[x][y] == '1')
+				return (step);
+			step++;
+		}
+		if (keycode == 0)
+			x--;
+		if (keycode == 2)
+			x++;
+	}
+	return (0);
+}
+
+int vertical_control(int keycode, t_vars *vars)
+{
+	int x;
+	int y;
+	int	step;
+
+	step = 0;
+	x = vars->player_x;
+	while (vars->map[x])
+	{
+		y = vars->player_y;
+		while (vars->map[x][y] != 1)
+		{
+			if (vars->map[x][y] == '1')
+				return (step);
+			step++;
+		}
+		if (keycode == 13)
+			y--;
+		if (keycode == 1)
+			y++;
+	}
 	return (0);
 }
 
@@ -57,9 +108,9 @@ int	control_format(char	*argv)
 		return (0);
 	while (argv[i] != '.')
 		i++;
-	if (argv[i + 1] != 'b' && argv[i + 2] != 'e' && argv[i + 3] != 'r' && argv[i + 4] != '\0')
-		return (1);
-	return (0);
+	if (argv[i + 1] != 'b' || argv[i + 2] != 'e' || argv[i + 3] != 'r' || argv[i + 4] != '\0')
+		return (0);
+	return (1);
 }
 
 int	main(int argc, char **argv)
@@ -68,27 +119,19 @@ int	main(int argc, char **argv)
 	
 	if (argc == 2)
 	{
-		printf("ciao1\n");
 		start(&vars, argv[1]);
     	if (ft_control(&vars) == 0)
        		return(0);
-		printf("control is ok\n");
-		
-		get_sprites(&vars);
-		printf("ciao8\n");
-		printf("%p\n", &vars);
-		draw_map(&vars);
-		printf("%p\n", &vars);
-		printf("ciao9\n");
-		
 		mlx_hook(vars.win, 2, 1L<<0, key_close, &vars);
 		mlx_hook(vars.win, 17, 0, xclose, &vars);
+		get_sprites(&vars);
+		draw_map(&vars);
 		mlx_loop(vars.mlx);
 	}
 	else
 	{
 		ft_putstr("Error:\nsomething looks wrong, please try again!");
-		exit(1);
+		exit (1);
 	}
 }
 
